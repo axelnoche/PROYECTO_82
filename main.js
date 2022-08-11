b4b827641eaae622d3132a4cd4a5203eb5aa9630
axelnoche <105469738+axelnoche@users.noreply.github.com>
2022-08-11 17:28:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("mi primer proyecto en consola");
+var a=10;
+var p = 5;
+
+var suma = a + p;
+console.log(suma);
+
+var resta = a - p;
+console.log(resta);
+
+var multiplicacion = a * p;
+console.log(multiplicacion);
+
+var division = a / p;
+console.log(division);
